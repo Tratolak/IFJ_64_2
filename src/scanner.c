078543750bd7e0409efc4364436aadb681fd6796
LAPTOP-QULSTDR0\Sekaczek <stomartin12@gmail.com>
2017-10-24 10:47:35 +0200

    scanner init

--- src/scanner.c
+++ src/scanner.c
@@ -1,1 +1,23 @@
 #include "scanner.h"
+
+int SOpenFile(char* path)
+{
+    SOURCE = fopen(path, "r");
+
+    if(SOURCE == NULL)
+    {
+        return S_FAILEDOPEN;
+    }
+
+    return 0;
+}
+
+Token GetToken()
+{
+
+}
+
+void SCloseFile()
+{
+    fclose(SOURCE);
+}
--- src/scanner.h
+++ src/scanner.h
@@ -1,8 +1,16 @@
 #ifndef SCANNER_H_INCLUDED
 #define SCANNER_H_INCLUDED
 
+#include <ctype.h>
+#include <stdio.h>
 #include "syntax_analyzer.h"
 
-int Scanner(char* InputPath);
+#define S_FAILEDOPEN 1
+
+FILE* SOURCE;
+
+int SOpenFile(char* path);
+Token GetToken();
+void SCloseFile();
 
 #endif // SCANNER_H_INCLUDED
