c508e3c88c18c2101c19e0daa38cb515e03d6ff3
ondraol <ondra.olsak@gmail.com>
2017-11-12 17:29:14 +0100

    if, while generating

--- src/code_generator.c
+++ src/code_generator.c
@@ -1,29 +1,80 @@
 #include "code_generator.h"
+#include "stack_operations.h"
 #include <stdio.h>
-
+#include <malloc.h>
 
 FILE *IFJcode17;
+tStack *levelStack;
 
 /**
  * Otevre soubor pro zapis generovaneho kodu a vlozi do nej uvodni
  * radek i s odradkovanim.
  *
- * @return false - error | true - success
+ * @return false - error | true - ok
  */
-bool fileOpen(){
-    IFJcode17 = fopen("IFJcode17","w");
-    if (IFJcode17 == NULL){
+bool fileOpen() {
+    IFJcode17 = fopen("IFJcode17", "w");
+    if (IFJcode17 == NULL) {
         return false;
-    }else{
-        fputs(".IFJcode17\n",IFJcode17);
+    } else {
+        fputs(".IFJcode17\n", IFJcode17);
+        levelStack = (tStack *) malloc(sizeof(tStack));
         return true;
     }
 }
 
+/**
+ * Vestavena fce na zjisteni delky retezce.
+ */
+void inbuildLenght() {
+    fputs("STRLEN /*vysledek (int)*/ /*retezec (string)*/\n", IFJcode17);
+}
 
+/**
+ * Vlozeni zacatku i konce cyklu.
+ */
+void loop() {
+    char stackTmp;
+    if (/*begin of while*/) {
+        stackPush(levelStack, /*begin of while*/);
+        fputs("LABEL /*any name*/\n", IFJcode17);
+        fputs("/*condition*/ /*when*/ /*endOfWhileCycle*/\n", IFJcode17);
+    } else {
+        stackTop(levelStack, &stackTmp);
+        if (stackTmp == /*end of while*/) {
+            fputs("JMP /*any name*/\n", IFJcode17);
+            fputs("LABEL /*any ENDname*/\n", IFJcode17);
+        }
+    }
+}
+/**
+ * Vlozeni uplne i castecne pominky.
+ */
+void ifCommand() {
+    char stackTmp;
+    //Zacatek podminky
+    if (/*begin of if*/) {
+        stackPush(levelStack, /*begin of while*/);
+        fputs("/*conditional jump to else/end of if*/\n", IFJcode17);
+    // Uplna podminka ELSE
+    } else if (stackTmp == /*else*/) {
+        stackTop(levelStack, &stackTmp);
+        if (stackTmp == /*begin of if*/) {
+            fputs("JMP /*end if*/\n", IFJcode17);
+            fputs("LABEL /*else*/\n", IFJcode17);
+            //Konec podminky
+        } else if (/*end if*/) {
+            fputs("LABEL /*end if*/\n", IFJcode17);
+        }
+    }
+}
+
+void function(){
+
+}
 /**
  * Zavre soubor pro zapis generovaneho kodu.
  */
-void fileClose(){
+void fileClose() {
     fclose(IFJcode17);
 }
--- src/code_generator.h
+++ src/code_generator.h
@@ -3,7 +3,10 @@
 #ifndef IFJ_64_2_CODE_GENERATOR_H
 #define IFJ_64_2_CODE_GENERATOR_H
 
-#endif //IFJ_64_2_CODE_GENERATOR_H
-
 bool fileOpen();
 void fileClose();
+void inbuildLenght();
+void loop();
+void ifCommand();
+void function();
+#endif //IFJ_64_2_CODE_GENERATOR_H
