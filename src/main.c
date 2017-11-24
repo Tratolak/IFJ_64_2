d47bfb7c9c97175e462c01aae3969db5fa1e0baf
xstodu08 <xstodu08@stud.fit.vutbr.cz>
2017-11-24 19:45:16 +0100

    useless code

--- src/main.c
+++ src/main.c
@@ -11,15 +11,6 @@ int main()
     //freopen("test.txt", "r", stdin);
     //printf("Hello world!\n");
     err=SyntaxAnalyzer();
-    switch (err){
-        case 1:
-            return 1;
-            break;
-        case 2:
-            return 2;
-            break;
-
-    }
 
     Symtable_Destroy();
     return err;
